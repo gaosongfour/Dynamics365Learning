06d4a8d5eda2faaf69b1877a424d46c11892926a
Song GAO <gaosongfour@hotmail.com>
2019-02-12 17:05:07 +0800

    add class ServerConfiguration and CrmServiceHelper

---
+++ Dynamics365Learning/ConnectToCRM/CrmServiceHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace ConnectToCRM
+{
+    public class CrmServiceHelper
+    {
+        /// <summary>
+        /// Get Crm OrganizationService For IFD On-premise
+        /// </summary>
+        /// <param name="config"></param>
+        public OrganizationServiceProxy GetServiceProxyIFD(ServerConfiguration config)
+        {
+            var serviceProxy = new OrganizationServiceProxy(config.OrganizationUri, config.HomeRealmUrl, config.Credentials, config.DeviceCredentials);
+            serviceProxy.EnableProxyTypes();
+            return serviceProxy;
+        }
+    }
+}
---
+++ Dynamics365Learning/ConnectToCRM/ServerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Description;
+
+namespace ConnectToCRM
+{
+
+    public class ServerConfiguration
+    {
+        public string ServerAddress { get; set; }
+        public string OrganizationName { get; set; }
+        public Uri DiscoveryUri { get; set; }
+        public Uri OrganizationUri { get; set; }
+        public Uri HomeRealmUrl { get; set; }
+        public ClientCredentials Credentials { get; set; }
+        public ClientCredentials DeviceCredentials { get; set; }
+        public bool UserSSL { get; set; }
+
+        public ServerConfiguration()
+        {
+            HomeRealmUrl = null;
+            DeviceCredentials = null;
+            Credentials = null;
+        }
+    }
+}
