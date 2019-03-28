1963be5182b91c24ce5de4a2472bdc92fda94f0c
Song GAO <gaosongfour@hotmail.com>
2019-03-28 18:05:44 +0800

    add method queryexpression with linkentity

--- Dynamics365Learning/CommonOperations/QueryOperation.cs
+++ Dynamics365Learning/CommonOperations/QueryOperation.cs
@@ -29,12 +29,17 @@ namespace CommonOperations
                 //Init crm service
                 var service = CrmServiceHelper.GetOrganizationServiceProxy(config);
 
-                //Basic Query Qxpression
-                RetrieveMultipleByQueryExpression(service);
+                //Basic Query Expression
+                //RetrieveMultipleByQueryExpression(service);
+
+                //Query Expression with LinkEntity
+                //RetrieveMultipleByQueryExpressionWithLinkEntity(service);
             }
             catch (FaultException<OrganizationServiceFault> ex)
             {
                 Console.WriteLine("OrganizationServiceFault Exception: " + ex.Message);
+                if (ex.InnerException != null)
+                    Console.WriteLine("Inner Exception: " + ex.InnerException.Message);
             }
             catch (Exception ex)
             {
@@ -81,6 +86,67 @@ namespace CommonOperations
             foreach (var entity in result.Entities)
                 Console.WriteLine("Print account: Guid:{0}; AccountName: {1}", entity.Id, entity.GetAttributeValue<string>("name"));
         }
+
+        /// <summary>
+        /// Retrieve Multiple with linkEntity
+        /// </summary>
+        /// <param name="service"></param>
+        public void RetrieveMultipleByQueryExpressionWithLinkEntity(IOrganizationService service)
+        {
+            var query = new QueryExpression()
+            {
+                EntityName = EntityName.Account,
+                ColumnSet = new ColumnSet(new string[] { "name" }),
+                Distinct = false,
+                Criteria = new FilterExpression()
+                {
+                    FilterOperator = LogicalOperator.And,
+                    Conditions =
+                    {
+                        new ConditionExpression("statecode", ConditionOperator.Equal,0)
+                    }
+                },
+                //Link entity =>Account.PrimaryContactid=Contact.ContactId
+                LinkEntities =
+                {
+                    new LinkEntity
+                    {
+                         EntityAlias="primarycontact",
+                         Columns =new ColumnSet("fullname"),
+                         JoinOperator= JoinOperator.Inner,
+                         LinkFromEntityName=EntityName.Account,
+                         LinkFromAttributeName="primarycontactid",
+                         LinkToEntityName=EntityName.Contact,
+                         LinkToAttributeName="contactid",
+                         LinkCriteria=
+                         {
+                            Conditions={
+                                new ConditionExpression("fullname", ConditionOperator.Like,"%Lyon%")
+                            }
+                         }
+                    }
+                },
+                Orders =
+                {
+                    new OrderExpression("name", OrderType.Descending)
+                }
+            };
+
+            var result = service.RetrieveMultiple(query);
+
+            //Output the result
+            Console.WriteLine("{0} accounts has/have been retrieved-------------", result.Entities.Count);
+            foreach (var entity in result.Entities)
+            {
+                var accountName = entity.GetAttributeValue<string>("name");
+                var contactFullname = string.Empty;
+                if (entity.Contains("primarycontact.fullname"))
+                    contactFullname = entity.GetAttributeValue<AliasedValue>("primarycontact.fullname").Value.ToString();
+                Console.WriteLine("Print account: Guid:{0}; AccountName: {1}; PrimaryContact: {2}", entity.Id, accountName, contactFullname);
+            }
+        }
         #endregion
+
+
     }
 }
