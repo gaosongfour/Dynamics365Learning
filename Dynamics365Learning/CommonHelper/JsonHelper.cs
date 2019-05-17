2a6b2907e6dff709452a56f7ac5ad4ba726c40d8
Song GAO <gaosongfour@hotmail.com>
2019-05-17 11:35:47 +0800

    add jsonhelper, to complete it

---
+++ Dynamics365Learning/CommonHelper/JsonHelper.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace CommonHelper
+{
+    public class JsonHelper
+    {
+        public static void PrintJsonResult(string jsonString)
+        {
+            var jsonResult = JObject.Parse(jsonString);
+            if(jsonResult.ContainsKey("value"))
+            {
+                var entityCollection = jsonResult["value"] as JArray;
+              
+                foreach(JObject entity in entityCollection)
+                {
+                    foreach(var prop in entity.Properties())
+                    {
+
+                    }
+
+                }
+            }
+        }
+    }
+}
